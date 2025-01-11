d111edc057eb6ad0cecf3ed84391c1e60eee669e
Gitaccount821 <nealvanzomeren@gmail.com>
2025-01-11 13:07:50 +0100

    first commit

---
+++ src/main/java/nl/novi/eindprojectbackend/EindprojectBackendApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.eindprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EindprojectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EindprojectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/novi/eindprojectbackend/EindprojectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.eindprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EindprojectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
