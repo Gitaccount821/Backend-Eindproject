348acebed9f28a360f28010df01666c3b712a666
Gitaccount821 <nealvanzomeren@gmail.com>
2025-01-24 21:02:54 +0100

    JWT token authenticatie toegevoegd (JWT token wordt gegenereerd), doormiddel van Authenticationcontroller, AuthenticationRequest, AuthenticationResponse, en JwtTokenUtil

---
+++ src/main/java/nl/novi/eindprojectbackend/controllers/AuthenticationController.java
@@ -0,0 +1,47 @@
+package nl.novi.eindprojectbackend.controllers;
+
+import nl.novi.eindprojectbackend.models.AuthenticationRequest;
+import nl.novi.eindprojectbackend.models.AuthenticationResponse;
+import nl.novi.eindprojectbackend.services.CustomUserDetailsService;
+import nl.novi.eindprojectbackend.utils.JwtTokenUtil;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/authenticate")
+public class AuthenticationController {
+
+    private final AuthenticationManager authenticationManager;
+    private final CustomUserDetailsService customUserDetailsService;
+    private final JwtTokenUtil jwtTokenUtil;
+
+    public AuthenticationController(AuthenticationManager authenticationManager,
+                                    CustomUserDetailsService customUserDetailsService,
+                                    JwtTokenUtil jwtTokenUtil) {
+        this.authenticationManager = authenticationManager;
+        this.customUserDetailsService = customUserDetailsService;
+        this.jwtTokenUtil = jwtTokenUtil;
+    }
+
+    @PostMapping
+    public AuthenticationResponse createAuthenticationToken(@RequestBody AuthenticationRequest request) {
+        try {
+
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword())
+            );
+
+
+            UserDetails userDetails = customUserDetailsService.loadUserByUsername(request.getUsername());
+            String jwt = jwtTokenUtil.generateToken(userDetails);
+
+            return new AuthenticationResponse(jwt);
+
+        } catch (AuthenticationException e) {
+            throw new RuntimeException("Invalid username or password.");
+        }
+    }
+}
