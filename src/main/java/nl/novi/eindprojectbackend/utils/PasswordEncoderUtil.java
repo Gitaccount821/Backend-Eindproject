f8177213d8b1b784806c3c7a03115a5d31c963f9
Gitaccount821 <nealvanzomeren@gmail.com>
2025-02-09 21:44:09 +0100

    installatiehandleiding done

--- src/main/java/nl/novi/eindprojectbackend/utils/PasswordEncoderUtil.java
+++ src/main/java/nl/novi/eindprojectbackend/utils/PasswordEncoderUtil.java
@@ -1,11 +1,11 @@
-package nl.novi.eindprojectbackend.config;
+package nl.novi.eindprojectbackend.utils;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class PasswordEncoderUtil {
     public static void main(String[] args) {
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        String rawPassword = "Klant2";
+        String rawPassword = "PASSWORD_HERE";
         String encodedPassword = passwordEncoder.encode(rawPassword);
         System.out.println("Encoded password: " + encodedPassword);
     }
